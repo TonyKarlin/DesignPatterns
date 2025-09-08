509571002c1a3734e6039f9754af0b5b9493cff9
TonyKarlin <tony.karlin@metropolia.fi>
2025-09-08 21:57:08 +0300

    Update card rank values for JACK, QUEEN, and KING to reflect correct scoring

--- Template/src/main/java/Game/Cards/Ranks.java
+++ Template/src/main/java/Game/Cards/Ranks.java
@@ -11,9 +11,9 @@ public enum Ranks {
     EIGHT(8),
     NINE(9),
     TEN(10),
-    JACK(10),
-    QUEEN(10),
-    KING(10);
+    JACK(11),
+    QUEEN(12),
+    KING(13);
 
     private final int value;
 
