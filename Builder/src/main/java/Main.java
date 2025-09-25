76b370bce636a3c2bf227f0ba2febe172377e008
TonyKarlin <tony.karlin@metropolia.fi>
2025-09-25 11:39:48 +0300

    Refactor main method to separate building logic for Office and Gaming computers

--- Builder/src/main/java/Main.java
+++ Builder/src/main/java/Main.java
@@ -5,11 +5,18 @@ import ConcreteClasses.Director;
 
 public class Main {
     public static void main(String[] args) {
-//        ComputerBuilder builder = new OfficeComputerBuilder();
-        ComputerBuilder builder = new GamingComputerBuilder();
-        Director director = new Director(builder);
-//        director.buildOfficeComputer();
-        director.buildGamingComputer();
-        builder.getComputer();
+        ComputerBuilder officeBuilder = new OfficeComputerBuilder();
+        ComputerBuilder gamingBuilder = new GamingComputerBuilder();
+        Director officeDirector = new Director(officeBuilder);
+        Director gamingDirector = new Director(gamingBuilder);
+
+
+        System.out.println("Building Office Computer:");
+        officeDirector.buildOfficeComputer();
+
+
+        System.out.println("\nBuilding Gaming Computer:");
+        gamingDirector.buildGamingComputer();
+
     }
 }
